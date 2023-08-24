b808ccd7351e8721a305fd9bb98706f56c8682d8
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-08-24 10:14:44 +0000

    Deploying to gh-pages from @ MarySovhyria/goit-js-hw-09@09c25032311c36881993d153021890605fa859d2 🚀

---
+++ 01-color-switcher.046340fa.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.046340fa.js.map
